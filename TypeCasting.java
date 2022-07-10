43f8851ba8a866fb2538ef387f21f4265f1fcd91
sujalin <102474797+sujalin@users.noreply.github.com>
2022-07-10 21:19:17 +0530

    Add files via upload

---
+++ TypeCasting.java
@@ -0,0 +1,12 @@
+class TypeCasting
+{
+ public static void main(String[] args)
+  {
+   char letter='Q';
+   int ascii=letter;
+   char alp=(char)ascii;
+   System.out.println("int value of letter " +ascii);
+   System.out.println("char value of letter " +alp);
+  }
+}
+   
