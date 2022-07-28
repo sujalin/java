cf86cd1780ab4864549caf7713eb896214d4a6e3
sujalin <102474797+sujalin@users.noreply.github.com>
2022-07-28 11:10:48 +0530

    Add files via upload

---
+++ Thursday Practicals/Practical1(07-07-2022)/DataType.java
@@ -0,0 +1,22 @@
+class DataType
+{
+ public static void main(String[] args)
+  { 
+    String name="Paul";
+    String brand="idol";
+    double processorSpeed=2.66;
+    double ramSize=8;
+    int harddiskSize=500;
+    int monitorSize=15;
+    double price=1760.55;
+    char servicePlan='C';
+    System.out.println("Name:-"+name);
+    System.out.println("Brand:-"+brand);
+    System.out.println("processor Speed In GHz:-"+processorSpeed);
+    System.out.println("Ram Size In GB:-"+ramSize);
+    System.out.println("Hard disk Size In GB:-"+harddiskSize);
+    System.out.println("Monitor Size:-"+monitorSize);
+    System.out.println("price:-"+price);
+    System.out.println("service Plan choosen:-"+servicePlan);
+  }
+}
