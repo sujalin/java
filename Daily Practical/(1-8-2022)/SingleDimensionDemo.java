2984d56766544f10a09ddebcc0c543b23efb9f57
sujalin <102474797+sujalin@users.noreply.github.com>
2022-08-01 23:14:04 +0530

    Add files via upload

---
+++ Daily Practical/(1-8-2022)/SingleDimensionDemo.java
@@ -0,0 +1,14 @@
+class SingleDimensionDemo
+{
+	public static void main(String[] args)
+	{
+		int[] a=new int[3];
+		a[0]=25;
+		a[1]=89;
+		a[2]=100;
+		System.out.println("Single dimension array elements are");
+		System.out.println(a[0]);
+		System.out.println(a[1]);
+		System.out.println(a[2]);
+	}
+}
